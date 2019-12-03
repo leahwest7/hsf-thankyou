4197fdc3f50889d234baef15ba001738eaaac2ab
Glitch (holiday-card-2020-proposal) <none>
2019-12-03 21:54:15 +0000

    ⛹👨‍👨‍👦 Checkpoint ./buttonHover.js:2246547/2259 ./style.css:2246547/603 ./index.html:2246547/393

--- buttonHover.js
+++ buttonHover.js
@@ -1,26 +1,25 @@
 const button = document.querySelector(".button");
-const buttonCoords = button.getBoundingClientRect();
 
 const clip = (min, max, value) => {
   return Math.max(min, Math.min(value, max));
-}
+};
 
 const boxShadow = (xOffset, yOffset) => {
-  const xBasis = xOffset / 10;
-  const yBasis = yOffset / 1000;
+  const xBasis = xOffset / 100;
+  const yBasis = yOffset / 100;
   const shadow1X = clip(-4, 4, xBasis * 1);
-  const shadow1Y = clip(-4, 4, yOffset * 1);
-  const shadow2X = clip(-4, 4, xBasis * 2);
-  const shadow2Y = clip(-4, 4, yOffset * 2);
-  const shadow3X = clip(-4, 4, xBasis * 3);
-  const shadow3Y = clip(-4, 4, yOffset * 3);
+  const shadow1Y = clip(-4, 4, yBasis * 1);
+  const shadow2X = clip(-8, 8, xBasis * 2);
+  const shadow2Y = clip(-8, 8, yBasis * 2);
+  const shadow3X = clip(-12, 12, xBasis * 3);
+  const shadow3Y = clip(-12, 12, yBasis * 3);
 
   return `${shadow1X}px ${shadow1Y}px 0px var(--blue-midtone), ${shadow2X}px ${shadow2Y}px 0px var(--red-highlight), ${shadow3X}px ${shadow3Y}px 0px var(--green-highlight)`;
 };
 
 const listenAction = ({ clientX, clientY }) => {
-  const xOffset = clientX - buttonCoords.left;
-  const yOffset = clientY - buttonCoords.top;
+  const xOffset = clientX - window.innerWidth / 2;
+  const yOffset = clientY - window.innerHeight / 2;
   const newBoxShadow = boxShadow(xOffset, yOffset);
   button.style.boxShadow = newBoxShadow;
 };
