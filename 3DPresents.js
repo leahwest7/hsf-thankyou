afea1c9db4bef1c7cd4cbc186aaef93e5f8346f7
Glitch (holiday-card-2020-proposal) <none>
2019-12-04 20:44:49 +0000

    🚊🎒 Checkpoint ./index.html:2246547/296 ./3DPresents.js:2246547/4930 ./script.js:2246547/744 ./style.css:2246547/66

--- 3DPresents.js
+++ 3DPresents.js
@@ -1,107 +1,117 @@
-let isSpinning = true;
+const generatePresent = ({
+  selector,
+  size,
+  highlight,
+  shadow,
+  ribbonHighlight,
+  ribbonShadow
+}) => {
+  let isSpinning = true;
+  const ribbonWidth = size / 7;
 
-let present = new Zdog.Illustration({
-  element: ".presents-1",
-  dragRotate: true,
-  onDragStart: () => (isSpinning = false),
-  rotate: {
-    x: -9
-  }
-});
+  let present = new Zdog.Illustration({
+    element: selector,
+    dragRotate: true,
+    onDragStart: () => (isSpinning = false),
+    rotate: {
+      x: -9
+    }
+  });
 
-const mainBox = new Zdog.Box({
-  addTo: present,
-  width: 100,
-  height: 100,
-  depth: 100,
-  stroke: false,
-  color: "#5B0F41",
-  leftFace: "#5B0F41",
-  rightFace: "#8C1746",
-  topFace: "#8C1746",
-  bottomFace: "#5B0F41"
-});
+  const mainBox = new Zdog.Box({
+    addTo: present,
+    width: size,
+    height: size,
+    depth: size,
+    stroke: false,
+    color: shadow,
+    rightFace: highlight,
+    topFace: highlight,
+  });
 
-// side ribbons
-const ribbon = new Zdog.Box({
-  addTo: mainBox,
-  width: 2,
-  height: 96,
-  depth: 15,
-  stroke: 4,
-  color: "#781356",
-  translate: {
-    x: -50
-  }
-});
+  // side ribbons
+  const ribbon = new Zdog.Box({
+    addTo: mainBox,
+    width: 2,
+    height: size - 4,
+    depth: ribbonWidth,
+    stroke: 4,
+    color: ribbonShadow,
+    translate: {
+      x: -size / 2
+    }
+  });
 
-ribbon.copy({
-  color: "#B8225E",
-  translate: {
-    x: 50
-  }
-});
+  ribbon.copy({
+    color: ribbonHighlight,
+    translate: {
+      x: size / 2
+    }
+  });
 
-ribbon.copy({
-  width: 15,
-  depth: 2,
-  translate: {
-    z: 50
-  }
-});
+  ribbon.copy({
+    width: ribbonWidth,
+    depth: 2,
+    translate: {
+      z: size / 2
+    }
+  });
 
-ribbon.copy({
-  width: 15,
-  depth: 2,
-  translate: {
-    z: -50
-  }
-});
+  ribbon.copy({
+    width: ribbonWidth,
+    depth: 2,
+    translate: {
+      z: -size / 2
+    }
+  });
 
-// top ribbons
-ribbon.copy({
-  width: 100,
-  height: 2,
-  depth: 15,
-  color: "#B8225E",
-  translate: {
-    y: -50
-  }
-});
+  // top ribbons
+  ribbon.copy({
+    width: size,
+    height: 2,
+    depth: ribbonWidth,
+    color: ribbonHighlight,
+    translate: {
+      y: -size / 2
+    }
+  });
 
-ribbon.copy({
-  width: 15,
-  height: 2,
-  depth: 100,
-  color: "#B8225E",
-  translate: {
-    y: -50
-  }
-});
+  ribbon.copy({
+    width: ribbonWidth,
+    height: 2,
+    depth: size,
+    color: ribbonHighlight,
+    translate: {
+      y: -size / 2
+    }
+  });
 
-// bottom ribbons
-ribbon.copy({
-  width: 100,
-  height: 2,
-  depth: 15,
-  translate: {
-    y: 50
-  }
-});
+  // bottom ribbons
+  ribbon.copy({
+    width: size,
+    height: 2,
+    depth: ribbonWidth,
+    translate: {
+      y: size / 2
+    }
+  });
 
-ribbon.copy({
-  width: 15,
-  height: 2,
-  depth: 100,
-  translate: {
-    y: 50
-  }
-});
+  ribbon.copy({
+    width: ribbonWidth,
+    height: 2,
+    depth: size,
+    translate: {
+      y: size / 2
+    }
+  });
 
-const animate = () => {
-  present.rotate.y += isSpinning ? 0.005 : 0;
-  present.updateRenderGraph();
-  requestAnimationFrame(animate);
+  const animate = () => {
+    present.rotate.y += isSpinning ? 0.005 : 0;
+    present.updateRenderGraph();
+    requestAnimationFrame(animate);
+  };
+
+  animate();
 };
 
-animate();
+export default generatePresent;
--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 import "./buttonHover.js";
 import "./snowAnimation.js";
-import "./3DPresents.js";
+import generatePresent from "./3DPresents.js";
+
+generatePresent({
+  selector: '.presents-1',
+  size: 140,
+  highlight: '#8C1746',
+  shadow: '#5B0F41',
+  ribbonHighlight: '#B8225E',
+  ribbonShadow: '#781356',
+});
+
+generatePresent({
+  selector: '.presents-2',
+  size: 55,
+  highlight: '#329F4A',
+  shadow: '#18744B',
+  ribbonHighlight: '#8C1746',
+  ribbonShadow: '#5B0F41',
+});
+
+generatePresent({
+  selector: '.presents-3',
+  size: 175,
+  highlight: '#4F33F3',
+  shadow: '#5B0F41',
+  ribbonHighlight: '#B8225E',
+  ribbonShadow: '#781356',
+});
